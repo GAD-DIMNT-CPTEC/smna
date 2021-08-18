33c0d6ede1643ff7c1b33be180f84020b5c1befe
João Gerd Zell de Mattos <joao.gerd@inpe.br>
2021-08-18 13:10:42 +0000

    Deletando diretório bam_scripts. A partir deste momento o script *run_model.sh* está apontando para os scripts dentro do diretório do modelo BAM. Então os scripts do BAM foram completamente adaptados para rodar no ciclo de assimilação, conforme descrito na tareda #10430 no repositório do BAM.

--- SMG/run/scripts/run_model.sh
+++ SMG/run/scripts/run_model.sh
@@ -3,8 +3,13 @@
 # Descomentar para debugar
 #set -o xtrace
 
+
 # Carregando as variaveis do sistema
 source /lustre_xc50/joao_gerd/SMG/config_smg.ksh vars_export
+
+# carregando funcoes do pre-processamento
+
+source ${home_run_bam}/runPre.func
 
 # Verificando argumentos de entrada
 if [ -z "${1}" ]
@@ -57,96 +62,75 @@ else
   export RUNPOS="yes"
 fi
 
+getBAMSize ${TRC}
+export postfix=$(printf "G%5.5dL%3.3d \n" $JM $NLV)
 export MRES=`echo ${TRC} ${NLV} | awk '{printf("TQ%4.4dL%3.3d\n",$1,$2)}'`
 export ANL="GANL${PREFIX}${LABELANL}S.unf.${MRES}"
 export LABELFGS=`${inctime} ${LABELANL} +6h %y4%m2%d2%h2`
 export YESTERDAY=$(${inctime} ${LABELANL} -1d %y4%m2%d200)
-export runmodel=${subt_model_bam_exec}_${PREFIX}
-export modelpos=${subt_pos_bam_exec}_${PREFIX}
+export modelDataIn=${subt_model_bam}/datain
+export gsiDataOut=${subt_gsi_dataout}/${LABELANL}
 
 echo -e ""
 echo -e "\033[34;1m >> Submetendo o MCGA:\033[m \033[31;1m${LABELANL}\033[m"
 echo -e " "
-echo -e "\033[34;1m > Resolucao         : \033[m \033[31;1m${MRES}\033[m"
-echo -e "\033[34;1m > Condicao Inicial  : \033[m \033[31;1m${LABELANL}\033[m"
-echo -e "\033[34;1m > Previsao ate      : \033[m \033[31;1m${LABELFCT}\033[m"
-echo -e "\033[34;1m > Pos-proc. Ativado : \033[m \033[31;1m${RUNPOS}\033[m"
+echo -e "\033[34;1m > Resolucao (espectral) : \033[m \033[31;1m${MRES}\033[m"
+echo -e "\033[34;1m > Resolucao (grade)     : \033[m \033[31;1m${postfix}\033[m"
+echo -e "\033[34;1m > Condicao Inicial      : \033[m \033[31;1m${LABELANL}\033[m"
+echo -e "\033[34;1m > Previsao ate          : \033[m \033[31;1m${LABELFCT}\033[m"
+echo -e "\033[34;1m > Pos-proc. Ativado     : \033[m \033[31;1m${RUNPOS}\033[m"
+
+#
+# mudando para diretorio dos scripts do BAM
+#
+
+cd ${home_run_bam}
+
+#
+# rodando o somente o Chopping do pré para pegar o arquivo de ozônio
+#
+
+
+/bin/bash runPre -v -t ${TRC} -l ${NLV} -I ${LABELANL} -s -n 2 -O
+
+mv -f ${modelDataIn}/OZONSMT${LABELANL}S.grd.${postfix} ${modelDataIn}/OZON${PREFIX}${LABELANL}S.grd.${postfix}
+
+#
+# remove arquivos desnecessarios
+#
+
+rm -fr ${modelDataIn}/GANLSMT${LABELANL}S.unf.*
+rm -fr ${modelDataIn}/OZONSMT${LABELANL}S.unf.*
 
 #
 # Copiando Analise do GSI para o Model/DataIn
 #
 
-cp -pfvr ${work_gsi_dataout}/${LABELANL}/GANL${PREFIX}${LABELANL}S.unf.${MRES} ${subt_model_bam_datain}
+cp -pfr ${gsiDataOut}/GANL${PREFIX}${LABELANL}S.unf.${MRES} ${modelDataIn}
 
-## Copiando/linkando demais arquivos do pré
 #
-## SST está disponível com 1 dia de atraso
-#ln -s ${ncep_ext}/${YESTERDAY}/dataout/NCEP/rtgssthr_grb_0.083.grib2.${YESTERDAY:0:8} ${subt_pre_bam_datain}/
+# Rodando os demais processos do pré e usando a análise do GSI
 #
-## Umidade do solo tem as 00UTC e as 12UTC
-#DATA=${LABELANL}
-#file="${ncep_ext}/${DATA}/dataout/Umid_Solo/GL_SM.GPNR.${DATA}.vfm"
-#c=0
-#until [ -e ${file} -o $c -gt 3 ]; do
-#   DATA=$(${inctime} ${DATA} -6h %y4%m2%d2%h2)
-#   file="${ncep_ext}/${DATA}/dataout/Umid_Solo/GL_SM.GPNR.${DATA}.vfm"
-#   c=$((c+1))
-#done
-#if [ ! -e ${file} ];then
-#   echo -ne "\033[32;1m Soil moisture file for\033[m: \033[34;1m${LABELANL}\033[m \033[32;1m Not Found\033[m"
-#   exit -1
-#else
-#   ln -s ${file} ${subt_pre_bam_datain}/GL_SM.GPNR.${LABELANL}.vfm
-#fi
 
-# Rodando o Pre 
-echo -ne "\033[34;1m > Pre-Processamento : \033[m"
-
-cd ${home_smg}/run/scripts/bam_scripts
-
-#echo "./runPre ${TRC} ${NLV} ${LABELANL} ${PREFIX} 0 F F 1534 64"
-#/bin/bash runPre ${TRC} ${NLV} ${LABELANL} ${PREFIX} 0 F F ${TRC} ${NLV} #> /dev/null 2>&1
-#/bin/bash runPre ${TRC} ${NLV} ${LABELANL} ${PREFIX} 0 F F 1534 64
-echo "/bin/bash runPre -t ${TRC} -l ${NLV} -I ${LABELANL} -p CPT -n 3 -ti 299 -li 64"
-/bin/bash runPre -t ${TRC} -l ${NLV} -I ${LABELANL} -p CPT -n 3 -ti 299 -li 64
-
-if [ $? -eq 0 ]
-then
-  echo -e "\033[34;1m [\033[m\033[32;2m OK \033[m\033[34;1m]\033[m"
-else
-  echo -e "\033[34;1m[\033[m\033[31;1m Falhou \033[m\033[34;1m]\033[m"
-  exit 1
-fi
+/bin/bash runPre -v -t ${TRC} -l ${NLV} -I ${LABELANL} -p CPT -n 3
 
 # Rodando o Modelo
-cd ${home_smg}/run/scripts/bam_scripts
 
-date
-
-echo ". runModel -np ${NPROC} -N 10 -d 4 -t ${TRC} -l ${NLV} -I ${LABELANL} -F ${LABELFCT} -W  ${LABELFCT} -p ${PREFIX} -s sstwkl -ts 3 -r -tr 6 -i -3"
-/bin/bash runModel -np ${NPROC} -N 10 -d 4 -t ${TRC} -l ${NLV} -I ${LABELANL} -F ${LABELFCT} -W  ${LABELFCT} -p ${PREFIX} -s sstwkl -ts 3 -r -tr 6 -i -3
-
-if [ $? -eq 0 ]
-then
- echo -e "\033[34;1m [\033[m\033[32;2m OK \033[m\033[34;1m]\033[m"
-else
-  echo -e "\033[34;1m[\033[m\033[31;1m Falhou \033[m\033[34;1m]\033[m"
-  exit 1
-fi
+/bin/bash runModel -das -v -np ${NPROC} -N 10 -d 4 -t ${TRC} -l ${NLV} -I ${LABELANL} -F ${LABELFCT} -W  ${LABELFCT} -p ${PREFIX} -s sstwkl -ts 3 -r -tr 6 -i -3
 
 # Pos-processa as previsoes caso a variavel RUNPOS possua o valor Yes ou Y
 if [ ${RUNPOS} == "yes" -o ${RUNPOS} == "y" ]
 then
 
   # Verifica se o executavel se encontra em ${subt_pos_bam_run}
-  if [ ! -e ${subt_pos_bam_run}/PostGrib ]
+  if [ ! -e ${subt_pos_bam}/exec/PostGrib ]
   then 
   
-    cp -v ${home_pos_bam_source}/PostGrib ${subt_pos_bam_run}
+    cp -v ${home_pos_bam}/source/PostGrib ${subt_pos_bam}/exec
   
   fi
 
-  cd ${home_smg}/run/scripts/bam_scripts
+  cd ${home_run_bam}
   echo   "./runPos -np 120 -N 12 -d 1 -t ${TRC} -l ${NLV} -I ${LABELANL} -F ${LABELFCT} -p ${PREFIX} > /dev/null 2>&1"
   /bin/bash runPos -np 120 -N 12 -d 1 -t ${TRC} -l ${NLV} -I ${LABELANL} -F ${LABELFCT} -p ${PREFIX} > /dev/null 2>&1
 fi
